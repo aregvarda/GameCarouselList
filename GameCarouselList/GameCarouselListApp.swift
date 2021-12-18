487eccec4240491815bdeba9b6b2015085e1ca05
Areg Vardanian <aregv@bk.ru>
2021-12-18 13:57:38 +0300

    Initial Commit

---
+++ GameCarouselList/GameCarouselListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCarouselListApp.swift
+//  GameCarouselList
+//
+//  Created by Геворг on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GameCarouselListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
