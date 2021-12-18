4b17be73c08033cd22021dcfe3fd464afe437610
Areg Vardanian <aregv@bk.ru>
2021-12-18 14:20:33 +0300

    finish init

--- GameCarouselList/ContentView.swift
+++ GameCarouselList/ContentView.swift
@@ -9,14 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
-
-struct Home : View {
-    
+struct Home: View {
     @State var search = ""
     @State var index = 0
     @State var columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
@@ -30,13 +27,176 @@ struct Home : View {
                         .fontWeight(.bold)
                     
                     Spacer()
-                        .padding(.horizontal)
-                    TextField("Search", text: self.$search)
-                        .padding(.vertical,10)
-                        .padding(.horizontal)
-                        .background(Color.black.opacity(0.07))
                 }
-                .padding(.vertical)
+                .padding(.horizontal)
+                
+                TextField("Search", text: $search)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .padding(.top, 25)
+                
+                // Carousel List
+                TabView(selection: $index) {
+                    ForEach(0...5, id: \.self) { index in
+                        Image("p\(index)")
+                            .resizable()
+                            // adding animations
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width - 35)
+                            //.frame(width: self.index == index ? 350 : 350, height: self.index == index ? 230 : 180)
+                            .cornerRadius(15)
+                            .scaleEffect(self.index == index ? 1.0 : 0.85)
+                            .padding(.horizontal)
+                            // for identifying current index
+                            .tag(index)
+                    }
+                }
+                .frame(height: 230)
+                .padding(.top, 25)
+                .tabViewStyle(PageTabViewStyle())
+                .animation(.easeOut)
+                
+                // adding custom Grid
+                
+                HStack {
+                    Text("Popular")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // reducing to row
+                        withAnimation(.easeOut) {
+                            if columns.count == 2 {
+                                columns.removeLast()
+                            } else {
+                                columns.append(GridItem(.flexible(), spacing: 15))
+                            }
+                        }
+                    }) {
+                        Image(systemName: columns.count == 2 ? "rectangle.grid.1x2": "square.grid.2x2")
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 25)
+                
+                LazyVGrid(columns: columns, spacing: 25) {
+                    ForEach(data) { game in
+                        // GridView
+                        GridView(game: game, columns: $columns)
+                    }
+                }
+                .padding([.horizontal, .top])
+            }
+            .padding(.vertical)
+        }
+        .background(
+            Color.black.opacity(0.05)
+            .edgesIgnoringSafeArea(.all)
+        )
+    }
+}
+
+struct GridView: View {
+    var game: Game
+    @Binding var columns: [GridItem]
+    @Namespace var namespace
+    
+    var body: some View {
+        VStack {
+            if columns.count == 2 {
+                VStack(spacing: 15) {
+                    ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                        Image(game.image)
+                            .resizable()
+                            .frame(height: 250)
+                            .cornerRadius(15)
+                        
+                        Button(action: {}) {
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(.red)
+                                .padding(.all, 10)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }
+                        .padding(.all, 10)
+                    }
+                    .matchedGeometryEffect(id: "image", in: namespace)
+                    
+                    Text(game.name)
+                        .fontWeight(.bold)
+                        .lineLimit(1)
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                    
+                    Button(action: {}) {
+                        Text("Buy Now")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 25)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    }
+                    .matchedGeometryEffect(id: "buy", in: namespace)
+                }
+            } else {
+                // Row View
+                
+                // adding animation
+                
+                HStack(spacing: 15) {
+                    ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                        Image(game.image)
+                            .resizable()
+                            .frame(width: (UIScreen.main.bounds.width - 45) / 2, height: 250)
+                            .cornerRadius(15)
+                        
+                        Button(action: {}) {
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(.red)
+                                .padding(.all, 10)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }
+                        .padding(.all, 10)
+                    }
+                    .matchedGeometryEffect(id: "image", in: namespace)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(game.name)
+                            .fontWeight(.bold)
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                        
+                        // Rating Bar
+                        HStack(spacing: 10) {
+                            ForEach(1...5, id: \.self) { rating in
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(game.rating >= rating ? .yellow : .gray)
+                            }
+                            
+                            Spacer(minLength: 0)
+                        }
+                        
+                        Button(action: {}) {
+                            Text("Buy Now")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 25)
+                                .background(Color.red)
+                                .cornerRadius(10)
+                        }
+                        .padding(.top, 10)
+                        .matchedGeometryEffect(id: "buy", in: namespace)
+                    }
+                }
+                .padding(.trailing)
+                .background(Color.white)
+                .cornerRadius(15)
             }
         }
     }
@@ -66,4 +226,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
