cac3e723a49992509bfeb19ef09c1536a4186014
Areg Vardanian <aregv@bk.ru>
2021-12-18 14:04:31 +0300

    Sample Model Data

--- GameCarouselList/ContentView.swift
+++ GameCarouselList/ContentView.swift
@@ -14,8 +14,37 @@ struct ContentView: View {
     }
 }
 
+
+struct Home : View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            
+        }
+    }
+}
+
+// Sample Model Data
+struct Game: Identifiable {
+    var id: Int
+    var name: String
+    var image: String
+    var rating: Int
+}
+
+var data = [
+    Game(id: 0, name: "Resident Evil 3", image: "g2", rating: 3),
+    Game(id: 1, name: "GTA 5", image: "g3", rating: 5),
+    Game(id: 2, name: "Assaisan Creed Odesey", image: "g4", rating: 3),
+    Game(id: 3, name: "Resident Evil 7", image: "g5", rating: 2),
+    Game(id: 4, name: "Watch Dogs 2", image: "g6", rating: 1),
+    Game(id: 5, name: "The Evil Within 2", image: "g7", rating: 2),
+    Game(id: 6, name: "Tomb Raider 2014", image: "g8", rating: 4),
+    Game(id: 7, name: "Shadow Of The Tomb Raider", image: "g1", rating: 4),
+]
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
